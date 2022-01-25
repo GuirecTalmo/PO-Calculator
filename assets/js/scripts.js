375448e579d5326baf8f8a7f5c8b1ed4a2a5832b
GuirecTalmo <guirec.talmo@gmail.com>
2022-01-25 10:17:21 +0100

    Ajout du bouton custom et favicon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,59 @@
 
 
+function getcustomValue() {
+    let Customtips = 0;
+    let askCustom = prompt('Which percent you want ?');
+    Customtips = Number(askCustom);
+    console.log(askCustom);
+    // Sélectionner l'élément input et récupérer sa valeur
+    let billAmount = document.getElementById("bill").value;
+    let peopleAmount = document.getElementById("people").value;
+
+      // Afficher la valeur
+      let totalTip = document.querySelector('.font_total_tip');
+      let totalPerson = document.querySelector('.font_total');
+  
+  
+      if (billAmount <= 0) {
+  
+          let invalidNumber = alert('You eat for free ?!')
+  
+          return invalidNumber
+          
+      }
+  
+      if (peopleAmount <= 0) {
+  
+          let invalidNumber = alert('Please enter a number of person')
+  
+          return invalidNumber
+          
+      }
+  
+      if (billAmount >= 1) {
+  
+          let tipsAmount = 0;
+          let totalAmount = 0;
+          console.log(billAmount);
+          console.log(Customtips);
+          tipsAmount = (billAmount / 100 * Customtips).toFixed(2);
+          console.log(tipsAmount);
+          document.getElementById("total_tip").innerHTML = ""; 
+          totalTip.append(tipsAmount);
+          totalAmount = (billAmount / peopleAmount).toFixed(2);
+          document.getElementById("total_person").innerHTML = ""; 
+          totalPerson.append(totalAmount);     
+      }
+
+}
+
 
 function getValue(ele) {
     // Sélectionner l'élément input et récupérer sa valeur
     let billAmount = document.getElementById("bill").value;
     let peopleAmount = document.getElementById("people").value;
-    let percentButton = ele.innerText;
+    let percentButton = ele.value; 
+
     // Afficher la valeur
     let totalTip = document.querySelector('.font_total_tip');
     let totalPerson = document.querySelector('.font_total');
@@ -42,7 +90,6 @@ function getValue(ele) {
         totalPerson.append(totalAmount);     
     }
 
-    return totalAmount;
 }
 
 function reset() {
@@ -54,11 +101,3 @@ function reset() {
     document.getElementById("total_person").innerHTML = "$0.00"; 
 
 }
-
-function calc() {
-   let calcul = (500 / 100) * 10; 
-   return calcul;
-}
-
-
-console.log(calc());
