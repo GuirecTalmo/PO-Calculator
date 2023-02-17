546c7e002bea37fdd5ddc1015c421a2041a8116b
Guirec Talmo <guirec.talmo@gmail.com>
2023-02-17 14:37:49 +0100

    [INTE] Refacto du script JS.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,103 +1,81 @@
-
-
-function getcustomValue() {
-    let Customtips = 0;
-    let askCustom = prompt('Which percent you want ?');
-    Customtips = Number(askCustom);
-    console.log(askCustom);
-    // Sélectionner l'élément input et récupérer sa valeur
-    let billAmount = document.getElementById("bill").value;
-    let peopleAmount = document.getElementById("people").value;
-
-      // Afficher la valeur
-      let totalTip = document.querySelector('.font_total_tip');
-      let totalPerson = document.querySelector('.font_total');
-  
-  
-      if (billAmount <= 0) {
-  
-          let invalidNumber = alert('You eat for free ?!')
-  
-          return invalidNumber
-          
-      }
-  
-      if (peopleAmount <= 0) {
-  
-          let invalidNumber = alert('Please enter a number of person')
-  
-          return invalidNumber
-          
-      }
-  
-      if (billAmount >= 1) {
-  
-          let tipsAmount = 0;
-          let totalAmount = 0;
-          console.log(billAmount);
-          console.log(Customtips);
-          tipsAmount = (billAmount / 100 * Customtips).toFixed(2);
-          console.log(tipsAmount);
-          document.getElementById("total_tip").innerHTML = ""; 
-          totalTip.append(tipsAmount);
-          totalAmount = (billAmount / peopleAmount).toFixed(2);
-          document.getElementById("total_person").innerHTML = ""; 
-          totalPerson.append(totalAmount);     
-      }
-
+// Cette fonction récupère la valeur de l'élément HTML par son id
+function getValueById(id) {
+  return Number(document.getElementById(id).value);
 }
 
-
-function getValue(ele) {
-    // Sélectionner l'élément input et récupérer sa valeur
-    let billAmount = document.getElementById("bill").value;
-    let peopleAmount = document.getElementById("people").value;
-    let percentButton = ele.value; 
-
-    // Afficher la valeur
-    let totalTip = document.querySelector('.font_total_tip');
-    let totalPerson = document.querySelector('.font_total');
-
-
-    if (billAmount <= 0) {
-
-        let invalidNumber = alert('You eat for free ?!')
-
-        return invalidNumber
-        
-    }
-
-    if (peopleAmount <= 0) {
-
-        let invalidNumber = alert('Please enter a number of person')
-
-        return invalidNumber
-        
-    }
-
-    if (billAmount >= 1) {
-
-        let tipsAmount = 0;
-        let totalAmount = 0;
-        console.log(billAmount);
-        console.log(percentButton);
-        tipsAmount = (billAmount / 100 * percentButton).toFixed(2);
-        console.log(tipsAmount);
-        document.getElementById("total_tip").innerHTML = ""; 
-        totalTip.append('$' + tipsAmount);
-        totalAmount = (billAmount / peopleAmount).toFixed(2);
-        document.getElementById("total_person").innerHTML = ""; 
-        totalPerson.append('$' + totalAmount);     
-    }
-
+// Cette fonction calcule le montant du pourboire
+function calculateTip(billAmount, tipPercentage) {
+  return (billAmount * tipPercentage) / 100;
 }
 
+// Cette fonction calcule le montant total par personne
+function calculateTotalPerPerson(billAmount, tipAmount, numberOfPeople) {
+  return (billAmount + tipAmount) / numberOfPeople;
+}
+
+// Cette fonction affiche le résultat dans l'élément HTML correspondant
+function displayResult(value, element) {
+  element.innerHTML = "$" + value.toFixed(2);
+}
+
+// Cette fonction réinitialise les valeurs des éléments HTML
 function reset() {
-    
-    document.getElementById("bill").value = "";
-    document.querySelector('.button_discount').value = "";
-    document.getElementById("total_tip").innerHTML = "$0.00"; 
-    document.getElementById("people").value = "";
-    document.getElementById("total_person").innerHTML = "$0.00"; 
-
+  document.getElementById("bill").value = "";
+  document.querySelector(".button_discount").value = "";
+  displayResult(0, document.getElementById("total_tip"));
+  document.getElementById("people").value = "";
+  displayResult(0, document.getElementById("total_person"));
 }
+
+// Cette fonction gère le clic sur les boutons de pourcentage de pourboire
+function handleClick(ele) {
+  const billAmount = getValueById("bill");
+  const numberOfPeople = getValueById("people");
+  if (billAmount <= 0 || numberOfPeople <= 0) {
+    alert("Please enter valid numbers");
+    return;
+  }
+  const tipPercentage = Number(ele.value);
+  const tipAmount = calculateTip(billAmount, tipPercentage);
+  const totalAmountPerPerson = calculateTotalPerPerson(
+    billAmount,
+    tipAmount,
+    numberOfPeople
+  );
+  displayResult(tipAmount, document.getElementById("total_tip"));
+  displayResult(totalAmountPerPerson, document.getElementById("total_person"));
+}
+
+// Cette fonction gère la soumission du formulaire de pourcentage de pourboire personnalisé
+function handleSubmit(event) {
+  event.preventDefault();
+  const billAmount = getValueById("bill");
+  const numberOfPeople = getValueById("people");
+  if (billAmount <= 0 || numberOfPeople <= 0) {
+    alert("Please enter valid numbers");
+    return;
+  }
+  const customPercentage = Number(event.target[0].value);
+  const tipAmount = calculateTip(billAmount, customPercentage);
+  const totalAmountPerPerson = calculateTotalPerPerson(
+    billAmount,
+    tipAmount,
+    numberOfPeople
+  );
+  displayResult(tipAmount, document.getElementById("total_tip"));
+  displayResult(totalAmountPerPerson, document.getElementById("total_person"));
+}
+
+// Écouteurs d'événements pour les boutons de pourcentage de pourboire
+const tipButtons = document.querySelectorAll(".button_discount");
+tipButtons.forEach((button) => {
+  button.addEventListener("click", () => handleClick(button));
+});
+
+// Écouteur d'événements pour le formulaire de pourcentage de pourboire personnalisé
+const customForm = document.getElementById("custom_form");
+customForm.addEventListener("submit", handleSubmit);
+
+// Écouteur d'événements pour le bouton de réinitialisation
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", reset);
